5dfd7969df257a5db71afbcd66f0bda1edcd1ff8
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-10-08 21:25:43 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -119,7 +119,7 @@ def game_hash
 end
 
 def longest_name
-  game_hash.select do |location, team_info|
+  game_hash.collect do |location, team_info|
     binding.pry
   team_info[:players].keys.flatten.max_by { |x| x.length }
   end
