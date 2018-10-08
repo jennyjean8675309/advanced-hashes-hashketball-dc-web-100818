07204201ac17e1f1bf733a1d76956ac73996aaf5
Jennifer Ingram <jennyjean8675309@yahoo.com>
2018-10-08 20:48:40 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -119,7 +119,7 @@ def game_hash
 end
 
 players = game_hash.collect do |location, team_info|
-    team_info[:players].keys.max { |a, b| a.length <=> b.length }
+    team_info[:players].keys.max_by { |x| x.length }
   end
   
   binding.pry
